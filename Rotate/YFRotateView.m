a1b200736febb661d3296bbfd71a97d1c294c372
颜风 <1178328901@qq.com>
2014-07-02 21:22:34 +0800

    修复BUG: 无法用视图”约束”描述轮转视图的子视图.

--- Rotate/YFRotateView.m
+++ Rotate/YFRotateView.m
@@ -33,79 +33,102 @@
 
 - (void) setupSubviews;
 {
-    if (nil == self.window) {
+    if (nil == self.window) { // 说明当前还不需要显示此视图,直接返回.
         return;
     }
     
-    UIView * headerView = [[UIView alloc] init];
-    headerView.backgroundColor = [UIColor blackColor];
-    [self addSubview: headerView];
-    
-    // ???: 迭代至此!滚动栏,无反应,或许应该检测另一个通知.
-    UIScrollView * viewContainer = [[UIScrollView alloc]init];
-    viewContainer.backgroundColor = [UIColor greenColor];
-    self.viewContainer.contentSize = CGSizeMake(self.frame.size.width * 2, 0);
-    viewContainer.pagingEnabled = YES;
-    [self addSubview: viewContainer];
-    
     /* 使用"约束"进行界面布局. */
-    
-    // ???: 如何动态获取导航栏高度?
-    // ???: 如何根据不同ios版本动态获取导航栏高度?
+    // ???: 如何动态获取导航栏高度?如何根据不同ios版本动态获取导航栏高度?
     NSNumber *  navHeight = [NSNumber numberWithFloat: 64.0]; //!< 导航栏高度.
     NSNumber * headerHeight = [NSNumber numberWithFloat: 20.0]; //!< 标题栏高度.
     
+    // 设置页眉.
+    UIView * headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor blackColor];
+    headerView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.headerView = headerView;
+    [self addSubview: self.headerView];
+    Release(headerView);
+    
+    // 设置视图容器.
+    UIScrollView * viewContainer = [[UIScrollView alloc]init];
+    viewContainer.backgroundColor = [UIColor greenColor];
+    viewContainer.contentSize = CGSizeMake(self.frame.size.width * 2, 0);
+    viewContainer.pagingEnabled = YES;
+    viewContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    viewContainer.delegate = self;
+    self.viewContainer = viewContainer;
+    Release(viewContainer);
+    [self addSubview: self.viewContainer];
+    
+    /* 设置视图容器的两个子视图及其约束.*/
+    UIView * firstView = [[UIView alloc] init];
+    firstView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.viewContainer addSubview: firstView];
+    Release(firstView);
+    
+    UIView * secondeView = [[UIView alloc] init];
+    secondeView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.viewContainer addSubview: secondeView];
+    Release(secondeView);
+    
+    // 设置视图间的约束.
     NSMutableArray * constraintsArray = [NSMutableArray arrayWithCapacity: 42];
-
+    
     [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"|[headerView]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(headerView)]];
+    
     [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"|[viewContainer]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(viewContainer)]];
     
     [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"V:|-navHeight-[headerView(==headerHeight)][viewContainer]|" options:0 metrics: NSDictionaryOfVariableBindings(navHeight, headerHeight) views: NSDictionaryOfVariableBindings(headerView,viewContainer)]];
+    [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"|[firstView(==320)][secondeView(==320)]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(firstView, secondeView)]];
+    
+    [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"V:|[firstView(>=300)]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(firstView)]];
+    
+    [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"V:|[secondeView(==firstView)]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(secondeView, firstView)]];
     
     [self addConstraints: constraintsArray];
-    
-    self.headerView = headerView;
-    Release(headerView);
-    self.headerView.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    self.viewContainer = viewContainer;
-    Release(viewContainer);
-    self.viewContainer.translatesAutoresizingMaskIntoConstraints = NO;
-    
-    
-    
-}
 
-- (void)setDataSource:(id<YFRotateViewDataSource>)dataSource
-{
-    /* 设置属性. */
-    [dataSource retain];
-    [_dataSource release];
-    _dataSource = dataSource;
+    
+    // !!!:临时设置的视图背景色
+    firstView.backgroundColor = [UIColor redColor];
+    secondeView.backgroundColor = [UIColor blueColor];
+
+//    [self.viewContainer addConstraints: constraintsOfViewContainer];
+    
+//    [self addConstraints: constraintsArray];
     
     /* 设置页面上初始显示的视图. */
     NSUInteger indexOfSetUpCell = 0;
-    if ([dataSource respondsToSelector: @selector(indexForSetupCellInRotateView:)]) {
-        indexOfSetUpCell = [dataSource indexForSetupCellInRotateView: self];
+    if ([self.dataSource respondsToSelector: @selector(indexForSetupCellInRotateView:)]) {
+        indexOfSetUpCell = [self.dataSource indexForSetupCellInRotateView: self];
         
-        if (indexOfSetUpCell > [dataSource numberOfCellsInRotateView:self]) {
+        if (indexOfSetUpCell > [self.dataSource numberOfCellsInRotateView:self]) {
             indexOfSetUpCell = 0;
         }
     }
-
     [self showCellAtIndex: indexOfSetUpCell];
 }
 
 - (void) showCellAtIndex: (NSUInteger) index
 {
+    
     // ???: 这个方法,好像很鸡肋.
     // ???:tableView中,代理方法是在设置代理之后立即执行吗?仔细看一下执行时机.
     // ???: 此处只能使用"约束"了!
     // ???: 约束会受bouds的影响吗?
     UIView * cell = [self.dataSource rotateView: self cellForColAtIndex: index];
-    cell.frame = CGRectMake(self.viewContainer.frame.size.width * index, 0, 320, 400);
-    CGRect rect = cell.frame;
+    cell.translatesAutoresizingMaskIntoConstraints = NO;
     [self.viewContainer addSubview: cell];
+
+    NSMutableArray * constraintsArray = [NSMutableArray arrayWithCapacity: 42];
+    CGRect frame = self.viewContainer.frame;
+    frame = self.viewContainer.bounds;
+    [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"H:[cell(==320)]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(cell)]];
+    [constraintsArray addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat: @"V:|[cell(==478)]|" options:0 metrics:nil views: NSDictionaryOfVariableBindings(cell)]];
+    
+    [self.viewContainer addConstraints:constraintsArray];
+//    cell.frame = CGRectMake(self.viewContainer.frame.size.width * index, 0, 320, 400);
+//    [self.viewContainer addSubview: cell];
 }
 
 # pragma mark - 协议方法
@@ -113,12 +136,12 @@
 {
     /* 获取视图位置. */
     // ???: 可能需要用到取整方法.
-    NSUInteger index = (scrollView.contentOffset.x / 320);
-    UIView * cell = [self.dataSource rotateView: self cellForColAtIndex: index];
-    // ???: 暂时先不用"约束"!请改用"约束".
-    // ???: 可以用leftView和rightView,固定位置.作为cell的父视图.
-    cell.frame = CGRectMake(scrollView.contentOffset.x, 0, scrollView.frame.size.width, scrollView.frame.size.height);
-    [scrollView addSubview: cell];
+//    NSUInteger index = (scrollView.contentOffset.x / 320);
+//    UIView * cell = [self.dataSource rotateView: self cellForColAtIndex: index];
+//    // ???: 暂时先不用"约束"!请改用"约束".
+//    // ???: 可以用leftView和rightView,固定位置.作为cell的父视图.
+//    cell.frame = CGRectMake(scrollView.contentOffset.x, 0, scrollView.frame.size.width, scrollView.frame.size.height);
+//    [scrollView addSubview: cell];
     
 //    CGFloat width = scrollView.frame.size.width; // 相册宽度.
 //    CGFloat x = scrollView.contentOffset.x; // 相册偏移.
